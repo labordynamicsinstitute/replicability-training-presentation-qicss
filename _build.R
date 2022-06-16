7c56ce4e7d0c253be5c7a259e095f3c2260902a9
Lars Vilhuber <lars.vilhuber@cornell.edu>
2022-06-16 14:32:57 -0400

    Adding additional info

--- _build.R
+++ _build.R
@@ -22,3 +22,4 @@ rmarkdown::render("part4.Rmd") # Command line/ Git/ Markdown
 rmarkdown::render("part5.Rmd")  # Data citations
 rmarkdown::render("part6.Rmd")  # Report
 rmarkdown::render("part7.Rmd")  # Test case
+rmarkdown::render("part8.Rmd")  # Resources
